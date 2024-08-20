9e86fa8fa1e8c8984c41f46871b393838c2655ad
angelaro93 <angela.ro.93@gmail.com>
2024-08-20 22:48:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+  const generateExcuse = () =>
+    `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(
+      when
+    )}`;
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
